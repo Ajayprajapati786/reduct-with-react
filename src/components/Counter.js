db0f99b3583c08c1d55fde6fa0fa744b05d70874
Ajay Prajapati <prajapatisani9@gmail.com>
2023-02-28 10:08:10 +0530

    till video 18

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,6 +5,7 @@ import classes from './Counter.module.css';
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
+  const show = useSelector(state =>state.showCounter);
 
   const increamentHandler = ()=>{
     dispatch({type:'increment'});
@@ -15,22 +16,29 @@ const Counter = () => {
   }
 
   const incrementBy5Handler = ()=>{
-    dispatch({type:'INCREMENTBY5'});
+    dispatch({type:'INCREMENTBY5', amount:5});
   }
 
   const decrementBy5Handler = ()=>{
     dispatch({type:'DECREMENTBY5'});
   }
 
-  const toggleCounterHandler = () => {};
+  const increaseHandler = () =>{
+    dispatch({type:'increase', amount:10});
+  }
+
+  const toggleCounterHandler = () => {
+    dispatch({type:'toggle'});
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && (<div className={classes.value}>{counter}</div>)}
       <div>
         <button onClick={increamentHandler}>Increament</button>
         <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={increaseHandler}>increment by 10</button>
         <button onClick={incrementBy5Handler}>IncrementBy5</button>
         <button onClick={decrementBy5Handler}>DecremenentBy5</button>
       </div>
--- src/store/Index.js
+++ src/store/Index.js
@@ -1,39 +1,58 @@
 import { createStore } from "redux";
 
-const counterReducer = (state ={counter:0},action) =>{
+const initialState = {counter:0, showCounter:true};
+const counterReducer = (state = initialState,action) =>{
     if(action.type === 'increment'){
         return {
-            counter:state.counter + 1
+            counter:state.counter + 1,
+            showCounter:state.showCounter
         }
     }
 
     if(action.type === 'decerement'){
         return {
-            counter:state.counter - 1
+            counter:state.counter - 1,
+            showCounter:state.showCounter
         }
     }
 
-    if(action.type === 'INCREMENTBY2'){
+    if(action.type === 'increase'){
         return {
-            counter:state.counter + 2
+            counter:state.counter + action.amount,
+            showCounter:state.showCounter
         }
     }
 
-    if(action.type === 'DECREMENTBY2'){
-        return {
-            counter:state.counter - 2
-        }
-    }
+    // if(action.type === 'INCREMENTBY2'){
+    //     return {
+    //         counter:state.counter + 2
+    //     }
+    // }
+
+    // if(action.type === 'DECREMENTBY2'){
+    //     return {
+    //         counter:state.counter - 2
+    //     }
+    // }
 
     if(action.type === 'INCREMENTBY5'){
         return {
-            counter:state.counter + 5
+            counter:state.counter + action.amount,
+            showCounter:state.showCounter
         }
     }
 
     if(action.type === 'DECREMENTBY5'){
         return {
-            counter:state.counter - 5
+            counter:state.counter - 5,
+            showCounter:state.showCounter
+        }
+    }
+
+    if(action.type === 'toggle'){
+        return {
+            counter:state.counter,
+            showCounter: !state.showCounter
         }
     }
 
